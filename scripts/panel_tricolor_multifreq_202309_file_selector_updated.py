89f30e26257a9d235fa88b345c018c1e26b46ca8
Valentina Staneva <vms16@uw.edu>
2023-10-19 03:37:37 +0000

    working for 2013 and 2019

--- scripts/panel_tricolor_multifreq_202309_file_selector_updated.py
+++ scripts/panel_tricolor_multifreq_202309_file_selector_updated.py
@@ -24,11 +24,12 @@ args, _ = parser.parse_known_args()
 
 zarr_file_input = args.zarr_file  # regridded zarr file to plot
 
-REGRID_FOLDER = "/home/ubuntu/efs/hake_nasc_202309/regridd_Sv_latlon/2013"
+REGRID_FOLDER = "/home/ubuntu/efs/hake_nasc_202309/regridd_Sv_latlon/"
 EVR_PRED_FOLDER = "/home/ubuntu/efs/hake_nasc_202309/prediction_evr"
-EVR_LABEL_FOLDER = "/home/ubuntu/efs/label_allocations/2013_test/staging/"
+EVR_LABEL_FOLDER = "/home/ubuntu/efs/label_allocations/"
+EVR_LABEL_FOLDER_pattern = "_run_2023_10_18/regions"
 
-file_selector = pn.widgets.FileSelector(REGRID_FOLDER, value=["/home/ubuntu/efs/hake_nasc_202309/regridd_Sv_latlon/2013/x0003_3_wt_20130614_160539_f0027_Sv_regridded_latlon.zarr"])
+
 
 # Determine which file to plot 
 if zarr_file_input is None:
@@ -38,61 +39,74 @@ else:
     # Use input zarr file
     zarr_file = Path(zarr_file_input)
 
+
+
+
+
+# file_selector = pn.widgets.FileSelector(REGRID_FOLDER, value=["/home/ubuntu/efs/hake_nasc_202309/regridd_Sv_latlon/2019/x0083_2_WT_20190815_204056_f0019_platform_Sv_latlon.zarr"])
+file_selector = pn.widgets.FileSelector(REGRID_FOLDER, value=[str(zarr_file)])
+
 print(f"Plotting: {zarr_file.name}")
 
 @pn.depends(file_selector.param.value, watch=False)
 def update_echogram(zarr_file):
 
-    filename = str(zarr_file[0]).split("regridd_Sv_latlon/")[-1].strip(".zarr")
+    filename = str(zarr_file[0]).split(REGRID_FOLDER)[-1].strip(".zarr")
 
     evr_file = Path(EVR_PRED_FOLDER) / f"{filename}_pred.evr"
 
     transect  = filename.split("/")[1].split("_")[0]
 
-    evr_manual_file = Path(EVR_LABEL_FOLDER) / f"{transect}_regions.evr"
+    year = filename.split("/")[0]
+
+    print(year)
+
+    evr_manual_file = Path(EVR_LABEL_FOLDER) / (year + EVR_LABEL_FOLDER_pattern) / f"{transect}_regions.evr"
+
+    print(evr_manual_file)
 
     ds_MVBS = xr.open_mfdataset(zarr_file[0], engine="zarr")
 
-    # Swap out depth or echo_range to the inverted echo_range dimension
-    # This is to circumvent the `y_increase` problem in holoviews
-    #ds_MVBS = (
-    #    ds_MVBS
-    #    .assign_coords({"depth_inverted": ("depth", ds_MVBS["depth"].values[::-1])})
-    #    .swap_dims({"depth": "depth_inverted"})
-    #    .drop(["depth", "echo_range"])
-    #    .rename({"depth_inverted": "echo_range"})
-    #)
+    # drop echo_range if it is there (it will be reassigned to depth)
+    if "echo_range" in list(ds_MVBS.coords):
+        ds_MVBS = (
+            ds_MVBS
+            .drop(["echo_range"])
+            )
 
     # swap depth with echo_range since echogram expects echo_range variable
     ds_MVBS = (
         ds_MVBS
-        .drop(["echo_range"])
         .assign_coords({"echo_range": ("depth", ds_MVBS["depth"].values)})
         .swap_dims({"depth": "echo_range"})
     )
-
-    # Plot tricolor echogram
-    #tricolor = ds_MVBS.eshader.echogram_multiple_frequency(
-    #    layout="composite",
-    #    rgb_map={
-    #        ds_MVBS.channel.values[2]: "R",  # 120 kHz
-    #        ds_MVBS.channel.values[1]: "G",  # 38 kHz
-    #        ds_MVBS.channel.values[0]: "B",  # 16 kHz
-    #    },
-    #    vmin=-70, vmax=-50
-    #)
-
+   
     gram_opts = opts.RGB(width=1200, height=500, xlabel="Time", ylabel="depth", title=filename)
 
+
+    # 2019 
+    #tricolor = ds_MVBS.eshader.echogram(
+    #    channel=[
+    #        'GPT 120 kHz 00907205a6d0 4-1 ES120-7C',
+    #        'GPT  38 kHz 009072060fbe 2-1 ES38B',
+    #        'GPT  18 kHz 009072058c8d 1-1 ES18-11',
+    #    ],
+    #    vmin = -100, 
+    #    vmax = -0,
+    #    rgb_composite = True,
+    #    opts = gram_opts,
+    #)
+    
+    # the channels strings seem to be different for different years: hence manual selection based on order 
+    channels = list(ds_MVBS.channel.values)
     tricolor = ds_MVBS.eshader.echogram(
         channel=[
-            'GPT 120 kHz 00907205a6d0 5-1 ES120-7C',
-            'GPT  38 kHz 009072058146 1-1 ES38B',
-            'GPT  18 kHz 009072058c8d 3-1 ES18-11',
-
+            channels[2],
+            channels[1],
+            channels[0],
         ],
         vmin = -70, 
-        vmax = -50,
+        vmax = -40,
         rgb_composite = True,
         opts = gram_opts,
     )
@@ -150,8 +164,6 @@ def update_echogram(zarr_file):
 
         # Plot region
         regions_manual = holoviews.Path(zip(ping_times_manual, depths_manual)).opts(color='k', line_width=2, xlim=(ds_MVBS.ping_time.min().values, ds_MVBS.ping_time.max().values))
-
-
 
 
     if regions_manual is not None and regions_pred is not None:
